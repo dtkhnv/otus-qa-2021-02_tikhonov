dacee7c1b5fe488fbb1e24136be1dd4d9244b864
Dmitriy Tikhonov <dstikhonov@rt-dc.ru>
2021-03-17 23:14:08 +0700

    HomeWork#4. Написаны тесты для проверки имени героев

--- HomeWork#3/src/homework/homework4.js
+++ HomeWork#3/src/homework/homework4.js
@@ -4,8 +4,38 @@
  * @returns {string}
  */
 
+export const kolobok = (name) => {
+        switch (name) {
+            case 'Дедушка':
+                return 'Я от дедушки ушел';
+                break;
+            case 'Бабушка':
+                return 'Я от бабушки ушел';
+                break;
+            case 'Заяц':
+                return 'Я от зайца ушел';
+                break;
+            case 'Волк':
+                return 'Я от волка ушел';
+                break;
+            default:
+                throw new Error(`Персонаж - ${name} не существует!`);
+        }
+};
+
 /**
- * kolobok
- * @param {string} name
+ * newYear
+ * @param {string} nameCharacter
  * @returns {string}
  */
+
+export const newYear = (nameCharacter) => {
+    switch (nameCharacter) {
+        case 'Снегурочка':
+        case 'Дед мороз':
+            return `${nameCharacter}! ${nameCharacter}! ${nameCharacter}!`;
+            break;
+        default:
+            throw new Error(`Персонаж - ${nameCharacter} не существует!`);
+    }
+};
